a8c617ab0e5e4b55a4e38bd5b9fc4a6e9b2916c5
Adit0507 <adi05072002@gmail.com>
2022-06-18 23:36:21 +0530

    Food order app files

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,37 @@
+import {useContext} from "react";
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
+
+const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {cartCtx.items.map((item) => (
+        <li> {item.name} </li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <Modal onClose={props.onClose} >
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span> {totalAmount} </span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems &&<button className={classes.button}>Order</button>}
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
