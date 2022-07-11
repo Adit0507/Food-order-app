cf905d664e6f4c84059af90278f8a1307b408368
Adit0507 <adi05072002@gmail.com>
2022-07-11 14:39:51 +0530

    Updating Cart Provider

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -34,21 +34,16 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
-
   if (action.type === 'REMOVE') {
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.id
     );
-
     const existingItem = state.items[existingCartItemIndex];
-    
     const updatedTotalAmount = state.totalAmount - existingItem.price;
     let updatedItems;
-    
     if (existingItem.amount === 1) {
       updatedItems = state.items.filter(item => item.id !== action.id);
-    } 
-    else {
+    } else {
       const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
       updatedItems = [...state.items];
       updatedItems[existingCartItemIndex] = updatedItem;
@@ -58,6 +53,10 @@ const cartReducer = (state, action) => {
       items: updatedItems,
       totalAmount: updatedTotalAmount
     };
+  }
+
+  if (action.type === 'CLEAR') {
+    return defaultCartState;
   }
 
   return defaultCartState;
@@ -77,11 +76,16 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: 'REMOVE', id: id });
   };
 
+  const clearCartHandler = () => {
+    dispatchCartAction({type: 'CLEAR'});
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler
   };
 
   return (
